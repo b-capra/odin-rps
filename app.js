337ca5041f81ac18ed7283fc7ba1b0567d172d03
b-capra <caprabmc@gmail.com>
2022-10-19 17:31:42 -0400

    Add functionality for individual rounds

--- app.js
+++ app.js
@@ -6,6 +6,44 @@
 // Repeat for 5 rounds (or until one player reaches 3?)
 
 function getCompChoice() {
-    const choices = ["Rock", "Paper", "Scissors"]
+    const choices = ["rock", "paper", "scissors"]
     return choices[Math.floor(Math.random()*choices.length)];
 }
+
+let compChoice = getCompChoice();
+
+let playerChoice = toLowerCase.prompt("Rock, Paper, or Scissors?:")
+
+function playRound(compChoice, playerChoice) {
+    if (compChoice === "rock") {
+        if (playerChoice === "scissors") {
+            return "You lose! Rock beats Scissors.";
+        } else if (playerChoice === "paper") {
+            return "You win! Paper beats Rock.";
+        } else if (playerChoice === "rock") {
+            return "It's a tie!";
+        }
+    } else if (compChoice === "paper") {
+        if (playerChoice === "rock") {
+            return "You lose! Paper beats Rock.";
+        } else if (playerChoice === "scissors") {
+            return "You win! Scissors beats Paper.";
+        } else if (playerChoice === "paper") {
+            return "It's a tie!";
+        }
+    } else if (compChoice === "scissors") {
+        if (playerChoice === "paper") {
+            return "You lose! Scissors beats Paper.";
+        } else if (playerChoice === "rock") {
+            return "You win! Rock beats Scissors.";
+        } else if (playerChoice === "scissors") {
+            return "It's a tie!";
+        }
+    }
+}
+
+
+
+console.log(compChoice);
+console.log(playerChoice);
+console.log(playRound(compChoice, playerChoice));
