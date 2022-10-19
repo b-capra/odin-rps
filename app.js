6fb258d4e579b7f9e831ea4bb9fa3fee73bebb96
b-capra <caprabmc@gmail.com>
2022-10-19 17:52:50 -0400

    Added Bo5 match functionality

--- app.js
+++ app.js
@@ -1,40 +1,46 @@
-// Generate choice for computer
-// Prompt user for choice
-// Play round by comparing choices
-// Tally score of overall match
-// Return string declaring round winner and match score
-// Repeat for 5 rounds (or until one player reaches 3?)
+let compScore = 0;
+let playerScore = 0;
 
 function getCompChoice() {
     const choices = ["rock", "paper", "scissors"]
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-let compChoice = getCompChoice();
+function addCompScore() {
+    compScore++;
+}
 
-let playerChoice = toLowerCase.prompt("Rock, Paper, or Scissors?:")
+function addPlayerScore() {
+    playerScore++;
+}
 
 function playRound(compChoice, playerChoice) {
     if (compChoice === "rock") {
         if (playerChoice === "scissors") {
+            addCompScore();
             return "You lose! Rock beats Scissors.";
         } else if (playerChoice === "paper") {
+            addPlayerScore();
             return "You win! Paper beats Rock.";
         } else if (playerChoice === "rock") {
             return "It's a tie!";
         }
     } else if (compChoice === "paper") {
         if (playerChoice === "rock") {
+            addCompScore();
             return "You lose! Paper beats Rock.";
         } else if (playerChoice === "scissors") {
+            addPlayerScore();
             return "You win! Scissors beats Paper.";
         } else if (playerChoice === "paper") {
             return "It's a tie!";
         }
     } else if (compChoice === "scissors") {
         if (playerChoice === "paper") {
+            addCompScore();
             return "You lose! Scissors beats Paper.";
         } else if (playerChoice === "rock") {
+            addPlayerScore();
             return "You win! Rock beats Scissors.";
         } else if (playerChoice === "scissors") {
             return "It's a tie!";
@@ -42,8 +48,23 @@ function playRound(compChoice, playerChoice) {
     }
 }
 
+function logWinner() {
+    if (playerScore === 3) {
+        console.log("Congratulations, you won!")
+    } else if (compScore === 3) {
+        console.log("Sorry, you lost.")
+    }
+}
 
+function game() {
+    for (let i = 3; playerScore < i && compScore < i;) {
+        let compChoice = getCompChoice();
+        let playerChoice = prompt("Rock, Paper, or Scissors?:").toLowerCase();
+        console.log(playRound(compChoice, playerChoice));
+        console.log(`The score is ${playerScore} - ${compScore}`)
+    }
 
-console.log(compChoice);
-console.log(playerChoice);
-console.log(playRound(compChoice, playerChoice));
+    logWinner();
+}
+
+game();
